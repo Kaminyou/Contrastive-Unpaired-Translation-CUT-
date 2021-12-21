5410c8e5e9645cf2a3b8c5bc9cae297b8b0a98d4
kaminyou <kaminyouho@aetherai.com>
2021-12-21 17:02:02 +0800

    resize

--- utils/util.py
+++ utils/util.py
@@ -6,7 +6,7 @@ from albumentations.pytorch import ToTensorV2
 
 transforms = A.Compose(
     [
-        A.Resize(width=256, height=256),
+        A.Resize(width=512, height=512),
         A.HorizontalFlip(p=0.5),
         A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255),
         ToTensorV2(),
@@ -16,7 +16,7 @@ transforms = A.Compose(
 
 test_transforms = A.Compose(
     [
-        A.Resize(width=256, height=256),
+        A.Resize(width=512, height=512),
         A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255),
         ToTensorV2(),
     ],
